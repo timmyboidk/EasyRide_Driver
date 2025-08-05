3f75029d68a49cdea3daf26664634ecbcb16ae17
Tim Zaiyang Yu <zaiyangyu369@gmail.com>
2025-08-05 15:42:33 +0800

    Add files via upload

---
+++ EasyRide_DriverTests/EasyRide_DriverTests.swift
@@ -0,0 +1,17 @@
+//
+//  EasyRide_DriverTests.swift
+//  EasyRide_DriverTests
+//
+//  Created by Tim Yu on 2025/8/5.
+//
+
+import Testing
+@testable import EasyRide_Driver
+
+struct EasyRide_DriverTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ EasyRide_DriverUITests/EasyRide_DriverUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  EasyRide_DriverUITestsLaunchTests.swift
+//  EasyRide_DriverUITests
+//
+//  Created by Tim Yu on 2025/8/5.
+//
+
+import XCTest
+
+final class EasyRide_DriverUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
